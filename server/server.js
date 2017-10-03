9ed0103e6a82e2da571c02d7441cb7d83f0cbc13
adsznzhang <adszn@hotmail.com>
2017-10-03 10:51:47 +0800

    Add test for delete

--- server/server.js
+++ server/server.js
@@ -62,7 +62,7 @@ app.delete('/todos/:id', (req, res) => {
         }
         res.send({todo});
     }).catch((e) => {
-        res.status(400).send();
+        res.status(400).send(e);
     });
 });
 
